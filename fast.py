b5c82452bd904be06d48158d218540028aafd6dc
abdulqadir952 <66480186+abdulqadir952@users.noreply.github.com>
2022-11-04 14:14:03 +0530

    Update fast.py

--- fast.py
+++ fast.py
@@ -15,30 +15,30 @@ async def Summation(info : Request):
         "status" : "SUCCESS",
         "Sum" : str(a+b)
     }
-@app.post("/getPieChart")
-async def getPieChart(info : Request):
-    cars = []
-    data = []
-    req_info = await info.json()
-    for i in range(len(req_info["Request"])):
-        cars.append(req_info["Request"][i]["Name"])
-        data.append(req_info["Request"][i]["Value"])
-    fig = plt.figure(figsize =(10, 7))
-    plt.pie(data, labels = cars)
-    img = io.BytesIO()              # create file-like object in memory to save image without using disk
-    plt.savefig(img, format='png')  # save image in file-like object
-    img.seek(0)
+# @app.post("/getPieChart")
+# async def getPieChart(info : Request):
+#     cars = []
+#     data = []
+#     req_info = await info.json()
+#     for i in range(len(req_info["Request"])):
+#         cars.append(req_info["Request"][i]["Name"])
+#         data.append(req_info["Request"][i]["Value"])
+#     fig = plt.figure(figsize =(10, 7))
+#     plt.pie(data, labels = cars)
+#     img = io.BytesIO()              # create file-like object in memory to save image without using disk
+#     plt.savefig(img, format='png')  # save image in file-like object
+#     img.seek(0)
 
-    return StreamingResponse(content=img, media_type="image/jpeg")
-    # return img.seek(0) 
-
-
+#     return StreamingResponse(content=img, media_type="image/jpeg")
+#     # return img.seek(0) 
 
 
-@app.get("/credentials")
-async def getCred():
-    obj=test.main()
-    return {
-        "status" : "SUCCESS",
-        "array" : obj
-    }
+
+
+# @app.get("/credentials")
+# async def getCred():
+#     obj=test.main()
+#     return {
+#         "status" : "SUCCESS",
+#         "array" : obj
+#     }
